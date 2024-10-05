6ea143dc03bdda020d8808c1aaa2f29293213aad
Yura <shupik3@gmail.com>
2024-10-05 10:07:43 +0300

    Create eho-bot.py

---
+++ eho-bot.py
@@ -0,0 +1,17 @@
+
+ # python -m pip install requests
+ # pip install pyTelegramBotAPI
+
+# ехо бот
+
+# import telebot
+
+# bot = telebot.TeleBot('7723550221:AAHpiu_NBKfo2l0OgkZgjkaGpf5RTVqBPF4')
+
+# @bot.message_handler(content_types=['text'])
+# def mybot(message):
+#   bot.send_message(message.chat.id, message.text)
+  
+# bot.polling(none_stop=True)
+
+
